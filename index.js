f32b27bbed8c6bcfb7c8dd2f11810ae95d26577b
Suyash Patil <suyash.p@fyle.in>
2025-02-16 14:25:35 +0530

    feat: add proper symentics

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   rules: {
     "one-interface-per-file": require("./rules/eslint-plugin-one-interface-per-file"),
     "one-enum-per-file": require("./rules/eslint-plugin-one-enum-per-file"),
+    "proper-semantics": require("./rules/eslint-plugin-proper-semantics"),
   },
 };
